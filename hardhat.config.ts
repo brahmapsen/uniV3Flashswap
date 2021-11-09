061c0f2173264686415ea2c84ff5fede09d61610
Brahma P Sen <aryasys@yahoo.com>
2021-11-09 04:23:13 -0500

    first commit

---
+++ hardhat.config.ts
@@ -0,0 +1,37 @@
+/**
+ * @type import('hardhat/config').HardhatUserConfig
+ */
+
+ import { config as dotEnvConfig } from "dotenv";
+ dotEnvConfig();
+
+ import "@nomiclabs/hardhat-etherscan";
+import "@nomiclabs/hardhat-waffle";
+import "@nomiclabs/hardhat-ethers";
+import '@typechain/hardhat'
+import "solidity-coverage"
+
+import "./tasks/accounts";
+import "./tasks/balance";
+import "./tasks/block-number";
+
+module.exports = {
+  solidity: "0.7.6",
+  paths: {
+    sources: "./contracts",
+    tests: "./test",
+    cache: "./cache",
+    artifacts: "./artifacts"
+  },
+  networks: {
+    hardhat: {
+      forking: {
+        url: process.env.ALCHEMY_MAINNET_RPC_URL,
+        blockNumber: 12975788
+      }
+    }
+  },
+  mocha: {
+    timeout: 200000
+  }
+};
